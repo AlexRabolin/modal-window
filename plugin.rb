64935243ca87a428133d1665d0fabe4bf18b6483
AlexRabolin <alexrabolin@yahoo.fr>
2016-07-04 09:27:15 +0200

    Add files via upload

---
+++ plugin.rb
@@ -0,0 +1,7 @@
+ #name: modal-window
+ #about: Allow markdown html code for opening a modal window from a post
+ #version: 0.1
+ #authors: @Alex_Rabolin
+
+ # whitelist html tag to open a modal window from a post forum Discourse
+register_asset 'javascripts/modal-window.js', :server_side
