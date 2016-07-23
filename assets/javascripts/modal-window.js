21ffbbc66f0a3e089aa0a5ebb2bf7793688cac43
AlexRabolin <alexrabolin@yahoo.fr>
2016-07-23 22:53:43 +0200

    Update modal-window.js

--- assets/javascripts/modal-window.js
+++ assets/javascripts/modal-window.js
@@ -1,5 +1,3 @@
 (function() {
 Discourse.Markdown.whiteListTag('label', 'for', '*');
-Discourse.Markdown.whiteListTag('span', 'style', '*');
-Discourse.Markdown.whiteListTag('label', 'for', '*')('span', 'style', '*');
 })();
